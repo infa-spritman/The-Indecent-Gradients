b22daf9b07581cf4769e3733b825191e6982c8f4
Koosh Doctor <kooshdoctor@Kooshs-MacBook-Pro.local>
2018-11-30 12:32:49 -0500

    Finished Chi Squared along with a method to match the two hashmaps

--- CategoricalAttributeAnalysis/src/main/java/AttributeAnalysis/ChiSquared.java
+++ CategoricalAttributeAnalysis/src/main/java/AttributeAnalysis/ChiSquared.java
@@ -3,11 +3,51 @@ package AttributeAnalysis;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ChiSquared {
     public static void main(String[] args) throws IOException {
+        HashMap<Integer, HashMap<String, HashMap<Integer, Double>>> expected = readFile(args[0]);
+        HashMap<Integer, HashMap<String, HashMap<Integer, Double>>> observed = readFile(args[1]);
+        completeTheHashMap(observed);
 
+        Double[] chiValues = new Double[expected.size() + 1];
+
+        for(Map.Entry<Integer, HashMap<String, HashMap<Integer, Double>>> entry0 : expected.entrySet()){
+            double chiValue = 0.0;
+            HashMap<String, HashMap<Integer, Double>> observedEntry0 = observed.get(entry0.getKey());
+            for(Map.Entry<String, HashMap<Integer, Double>> entry1 : entry0.getValue().entrySet()){
+                HashMap<Integer, Double> observedEntry1 = observedEntry0.get(entry1.getKey());
+
+                for(Map.Entry<Integer, Double> entry2 : entry1.getValue().entrySet()){
+                    Double observedEntry2 = observedEntry1.get(entry2.getKey());
+                    if(entry2.getValue() != 0)
+                        chiValue += Math.pow((observedEntry2 - entry2.getValue()),2)/entry2.getValue();
+                }
+            }
+            chiValues[entry0.getKey()] = chiValue;
+        }
+
+        PrintWriter write = new PrintWriter("chi.txt", "UTF-8");
+
+        for(int i = 1; i < chiValues.length; i++){
+            write.println(i + "," + chiValues[i-1]);
+        }
+        write.close();
+    }
+
+    public static void completeTheHashMap(HashMap<Integer, HashMap<String, HashMap<Integer, Double>>> map){
+        for(Map.Entry<Integer, HashMap<String, HashMap<Integer, Double>>> entry0: map.entrySet()){
+            for(Map.Entry<String, HashMap<Integer, Double>> entry1 : entry0.getValue().entrySet()){
+                for(int i = 0; i < 14; i++){
+                    if(!entry1.getValue().containsKey(i)){
+                        entry1.getValue().put(i, 0.0);
+                    }
+                }
+            }
+        }
     }
 
     public static HashMap<Integer, HashMap<String, HashMap<Integer, Double>>> readFile(String fileAddress) throws IOException {
